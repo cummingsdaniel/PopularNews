19313a14591ffe86bd566876faaae992fcc7c0ea
cummi <cummingsdan87@gmail.com>
2019-11-27 18:12:50 -0500

    PopularNews

---
+++ app/src/main/java/com/vogella/android/popularnews/api/ApiInterface.java
@@ -0,0 +1,16 @@
+package com.vogella.android.popularnews.api;
+
+import com.vogella.android.popularnews.models.News;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+
+    @GET("top-headlines")
+    Call<News> getNews(
+            @Query("country") String country ,
+            @Query("apiKey") String apiKey
+    );
+}
