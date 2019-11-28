72d20b61461e07078dc3dd6839b510b9ccca29cd
cummi <cummingsdan87@gmail.com>
2019-11-27 23:23:02 -0500

    PopularNews

--- app/src/main/java/com/vogella/android/popularnews/Adapter.java
+++ app/src/main/java/com/vogella/android/popularnews/Adapter.java
@@ -1,6 +1,8 @@
 package com.vogella.android.popularnews;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -9,30 +11,81 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
 
 import com.vogella.android.popularnews.models.Article;
 
 import java.util.List;
 
-public class Adapter extends RecyclerView.Adapter{
+public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder>{
     private List<Article>articles;
     private Context context;
-    private AdapterView.OnItemClickListener onItemClickListener;
+    private OnItemClickListener onItemClickListener;
+
+
+    public Adapter(List<Article> articles, Context context) {
+        this.articles = articles;
+        this.context = context;
+    }
+
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item, parent, false);
+        return new MyViewHolder(view, onItemClickListener);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holders, int position) {
+        final MyViewHolder holder = holders;
+        Article model =articles.get(position);
 
+        RequestOptions requestOptions = new RequestOptions();
+        requestOptions.placeholder(Utils.getRandomDrawbleColor());
+        requestOptions.error(Utils.getRandomDrawbleColor());
+        requestOptions.diskCacheStrategy(DiskCacheStrategy.ALL);
+        requestOptions.centerCrop();
+
+        Glide.with(context)
+                .load(model.getUrlToImage())
+                .apply(requestOptions)
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        holder.progressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        holder.progressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+                })
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(holder.imageView);
+
+        holder.title.setText(model.getTitle());
+        holder.desc.setText(model.getDescription());
+        holder.source.setText(model.getSource().getName());
+        holder.time.setText(" \u2022 " + Utils.DateToTimeFormat(model.getPublishedAt()));
+        holder.published_ad.setText(Utils.DateFormat(model.getPublishedAt()));
+        holder.author.setText(model.getAuthor());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return articles.size();
     }
 
     public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
@@ -70,7 +123,7 @@ public class Adapter extends RecyclerView.Adapter{
         }
         @Override
         public void onClick(View v) {
-
+            onItemClickListener.onItemClick(v, getAdapterPosition());
         }
     }
 
